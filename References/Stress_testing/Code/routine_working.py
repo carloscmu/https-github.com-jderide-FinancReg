f3a901328b6bb08397265f8f82e5b1d84869f425
jderide <julio.deride@gmail.com>
2018-02-28 22:26:03 -0700

    new routine

--- References/Stress_testing/Code/routine_working.py
+++ References/Stress_testing/Code/routine_working.py
@@ -4,9 +4,6 @@ from numpy import linalg as LA
 import numpy as np
 import nlopt
 
-plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-plt.rc('text', usetex=True)
-
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
@@ -24,7 +21,7 @@ def cpopt(x,grad):
 	s = 0.0
 	N = np.size(x)
 	for n in range(0,N):
-		s = s+epiopt(x,lopt[n])
+		s = s+epiopt(x[n],lopt[n])
 	return s
 
 
@@ -56,17 +53,33 @@ S = (Saux+np.eye(npts)).dot(q)
 lopt = lbda/(1.0-S*UB)
 ru = a0/(a1*(1.0-exe))
 
-NGrid = 1000
-xx = np.linspace(0.0,1.0,NGrid)
-ff = xx*0.0
-for k in range(0,NGrid):
-	ff[k] = cpopt(xx[k],0)
+x0 = np.ones(npts)*(lbda-0.05)
 
-plt.plot(xx,ff,linewidth=3.0)
-plt.title(r'{\rm CP - Optimal expected Utility }$\mathbf{E}\{\pi(r^*(x))\}$')
-plt.xlabel(r'$x$')
-plt.ylabel(r'$\mathbf{E}\{\sum_{i}\pi(r^*(x))\}$')
-plt.savefig('CPExpUt.eps')
+opt = nlopt.opt(nlopt.LN_BOBYQA, npts)
+opt.set_lower_bounds(np.zeros(npts))
+opt.set_upper_bounds(np.ones(npts))
+opt.set_max_objective(cpopt)
+opt.set_xtol_rel(1e-8)
+xopt = opt.optimize(x0)
+minf = opt.last_optimum_value()
+print("optimum at ", xopt)
+status_sol=opt.last_optimize_result()
+p_s='Code '
+if status_sol <= 0.0:
+	p_s=p_s+'Error'
+elif status_sol ==  1:
+	p_s=p_s+'Generic success return value.'
+elif status_sol == 2:
+	p_s=p_s+'Optimization stopped because stopval (above) was reached.'
+elif status_sol == 3:
+	p_s=p_s+'Optimization stopped because ftol_rel or ftol_abs (above) was reached.'
+elif status_sol == 4:
+	p_s=p_s+'Optimization stopped because xtol_rel or xtol_abs (above) was reached.'
+elif status_sol == 5:
+	p_s=p_s+'Optimization stopped because maxeval (above) was reached.'
+else:
+	p_s=p_s+'Optimization stopped because maxtime (above) was reached.'
+print "Result code = ", p_s
 
 
 ##########################################################################################3
